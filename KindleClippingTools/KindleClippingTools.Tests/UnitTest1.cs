ea94459432e124579352df5a9aab3f41507a9121
Mariusz Klimek <klimcio@gmail.com>
2021-07-19 08:00:42 +0200

    Beginning of Unit Tests

---
+++ KindleClippingTools/KindleClippingTools.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace KindleClippingTools.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Testing_if_one_clipping_file_will_return_list_of_one_element()
+        {
+
+        }
+    }
+}
