3a6cbeaabd347ffcf0672a89af6614e12d1385a9
Mariusz Klimek <klimcio@gmail.com>
2021-07-31 21:20:19 +0200

    Implement class that is parsing clipping files

---
+++ KindleClippingTools/KindleClippingTools.Logic/KindleClippingParser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace KindleClippingTools.Logic
+{
+    public class KindleClippingParser : IParseClippingFiles
+    {
+        public List<Clipping> ParseFile(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
